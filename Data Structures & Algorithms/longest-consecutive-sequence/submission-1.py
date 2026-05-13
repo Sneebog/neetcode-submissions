30cea10e2891cd3f8864e710ebf236fc4d9c4428
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-05-13 21:53:11 +0100

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_hashmap = {}
+        max_sequence = 0
+        
+        for i in range(0, len(nums)):
+            if num_hashmap.get(nums[i] - 1) is not None:
+                num_hashmap[nums[i]-1] = nums[i]
+
+            if num_hashmap.get(nums[i] + 1) is not None:
+                num_hashmap[nums[i]] = nums[i] + 1
+            else:
+                num_hashmap[nums[i]] = -1
+
+        loop_count = 0
+        sequence_count = 0
+        # for num, next_num in num_hashmap.items():
+        #     if num + 1 = next_num:
+        while loop_count < len(nums):
+            if num_hashmap.get(nums[loop_count] + sequence_count) is not None:
+                sequence_count += 1
+            else:
+                if sequence_count > max_sequence:
+                    max_sequence = sequence_count
+                sequence_count = 0
+                loop_count += 1
+        
+        return max_sequence
+
+
+            
+
