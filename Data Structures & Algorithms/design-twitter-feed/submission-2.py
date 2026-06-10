a905dc907393b67317b59429bca517d652b4c43f
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-06-10 11:53:28 +0100

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,33 @@
+class Twitter:
+
+    def __init__(self):
+        #hashmap for user id to queue of tweets
+        self.user_map = {}
+        self.recent_tweets = []
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.user_map[userId] = self.user_map.get(userId, set())
+        self.recent_tweets.append((userId,tweetId))
+  
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        i = len(self.recent_tweets) - 1
+        user_follows = self.user_map[userId]
+
+        tweets = []
+        while i >= 0 and len(tweets) < 10:
+            if self.recent_tweets[i][0] in user_follows or self.recent_tweets[i][0] == userId :
+                tweets.append(self.recent_tweets[i][1])
+            i -= 1
+        return tweets
+
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.user_map[followerId] = self.user_map.get(followerId, set())
+        self.user_map[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.user_map[followerId] = self.user_map.get(followerId, set())
+        if self.user_map[followerId] and followeeId in self.user_map[followerId]:
+            self.user_map[followerId].remove(followeeId)
