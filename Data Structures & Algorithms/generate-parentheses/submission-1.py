a81612bc15fc33a05b13cfc052f0df20bc16f482
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-06-14 23:00:38 +0100

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = set()
+
+        def paren(cnt, par):
+            if cnt == n:
+                res.add(par)
+                return
+            #either insert in the position or don't?
+            for i in range(0, len(par)):
+                new_par = par[:i] + "()" + par[i:]
+                paren(cnt+ 1, new_par)
+    
+        paren(1, "()")
+        return [paren for paren in res]
+
+            
