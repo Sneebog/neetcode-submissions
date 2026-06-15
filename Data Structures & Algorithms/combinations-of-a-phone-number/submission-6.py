a4a3754cd0b80b2c1e2456502a57b312b4cae417
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-06-15 18:38:48 +0100

    Add: combinations-of-a-phone-number - submission-6

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        res = []
+        #hash map
+        digit_map = {
+            2: ("a", "b", "c"),
+            3: ("d", "e", "f"),
+            4: ("g", "h", "i"),
+            5: ("j", "k", "l"),
+            6: ("m", "n", "o"),
+            7: ("p", "q", "r", "s"),
+            8: ("t", "u", "v"),
+            9: ("w", "x", "y", "z"),
+        }
+        
+        def add_digit(cnt,word):
+            if cnt == len(digits):
+                res.append(word)
+                return 
+            
+            #go through every possible character
+            num = int(digits[cnt])
+            chars = digit_map[num]
+            for i in range(0, len(chars)):
+                tmp_word = word + chars[i]
+                add_digit(cnt + 1, tmp_word)
+        add_digit(0, "")
+        return res
+
+            
