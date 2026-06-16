671f83e159e865da090e19ff0724ad1618d204cd
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-06-16 12:05:43 +0100

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res  = set()
+        subset = []
+
+        def palin(l, r):
+            while l < r:
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            return True
+        
+        def check(cnt):
+            if cnt >= len(s):
+                res.add(tuple(subset))
+            
+            for i in range(cnt, len(s)):
+                if palin(cnt,i):
+                    subset.append(s[cnt:i+1])
+                    check(i + 1)
+                    subset.pop()
+        check(0)
+        return [list(comb) for comb in res]
