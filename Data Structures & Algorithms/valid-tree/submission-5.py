26efaad5aeed2b1ae68eb60bd905a5890e44112a
SBaldrey <85622534+Sneebog@users.noreply.github.com>
2026-06-29 16:30:36 +0100

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+      if len(edges) < n - 1:
+        return False
+      edgemap = [[]  for i in range(n)]
+      for n1, n2 in edges:
+        edgemap[n1].append(n2)
+        edgemap[n2].append(n1)
+
+      tree = set()
+      def dfs(node, par):
+        #check if there is a cycle using the set 
+        if node in tree:
+          return False
+        
+        #no edges
+        tree.add(node)
+        for edge in edgemap[node]:
+          if edge == par:
+            continue
+          if not dfs(edge, node): 
+            return False
+
+        return True
+      return dfs(0, -1) and len(tree) == n
